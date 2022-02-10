d32c45aacd361e2b99aa47f6f08c7098e30a7713
Ralf Zeilstra <ralfzeils@gmail.com>
2022-02-10 14:14:34 +0100

    Practicum 2A

---
+++ Practicum2/Zwembad.java
@@ -0,0 +1,37 @@
+package Practicum2;
+
+public class Zwembad{
+
+    private double lengte;
+    private double breedte;
+    private double diepte;
+
+    public Zwembad( double lg, double br, double dp ){
+        lengte = lg;
+        breedte = br;
+        diepte = dp;
+    }
+
+    public Zwembad(){
+        lengte  = 0.0;
+        breedte = 0.0;
+        diepte  = 0.0;
+    }
+
+    //Setters
+    public void setLengte   (double lg) { lengte    = lg; }
+    public void setBreedte  (double br) { breedte   = br; }
+    public void setDiepte   (double dp) { diepte    = dp; }
+
+    //Getters
+    public double getLengte  () { return lengte;  }
+    public double getBreedte () { return breedte; }
+    public double getDiepte  () { return diepte;  }
+
+    //Calculate inhoud of the zwembad and return in
+    public double inhoud()  { return (lengte * breedte * diepte); }
+
+    public String toString(){
+        return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
+    }
+}
