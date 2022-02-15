68b1b7b56545b55fb905bd5329e0671efab26b41
Ralf Zeilstra <ralfzeils@gmail.com>
2022-02-15 13:41:49 +0100

    Added Practicum2B

---
+++ Practicum2B/Voetbalclub.java
@@ -0,0 +1,34 @@
+package Practicum2B;
+public class Voetbalclub{
+    private String clubNaam;
+
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+    public Voetbalclub(String nm){
+        clubNaam = nm;
+    }
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+          aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+          aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+          aantalVerloren = aantalVerloren + 1;
+      }
+
+    public int aantalPunten()       { return (aantalGewonnen * 3) + (aantalGelijk * 1); }
+    public int aantalGespeeld()     { return aantalGewonnen + aantalGelijk + aantalVerloren; }
+
+    public String toString() {
+        return String.format("%s %o %o %o %o %o", 
+                                clubNaam, 
+                                aantalGespeeld(), 
+                                aantalGewonnen, 
+                                aantalGelijk, 
+                                aantalVerloren, 
+                                aantalPunten());
+    }
+}
