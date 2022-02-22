b88de3e9cd47b1697be192d058988a2e22085450
Ralf Zeilstra <ralfzeils@gmail.com>
2022-02-22 15:51:08 +0100

    Test Verbeterd voor Practicum4B

--- Practicum4B/AutoHuurTest.java
+++ Practicum4B/AutoHuurTest.java
@@ -6,35 +6,75 @@ import static org.junit.jupiter.api.Assertions.*;
 class AutoHuurTest {
 
     @Test 
-    public void TestAutoHuurEnKorting() { 
-        //Checken of met geen input de naam FC word
+    public void KlantZonderKortingTest() { 
         Klant k1 = new Klant("John Doe");
-        Klant k2 = new Klant("Jane Doe");
-        k2.setKorting(50.0);
 
-        Auto a1 = new Auto("Audi", 50.0);
-        a1.setPrijsPerDag(300.0);
+        Auto a1 = new Auto("Audi", 300.0);
 
         AutoHuur ah1 = new AutoHuur();
         ah1.setHuurder(k1);
         ah1.setGehuurdeAuto(a1);
         ah1.setAantalDagen(100);
 
-        AutoHuur ah2 = new AutoHuur();
-        ah2.setHuurder(k2);
-        ah2.setGehuurdeAuto(a1);
-        ah2.setAantalDagen(100);
-
-
         assertEquals(30000, ah1.totaalPrijs());
-        assertEquals(15000, ah2.totaalPrijs());
+    } 
+
+    @Test 
+    public void KlantMetKortingTest() { 
+        Klant k1 = new Klant("John Doe");
+        k1.setKorting(50.0);
+
+        Auto a1 = new Auto("Audi", 300.0);
+
+        AutoHuur ah1 = new AutoHuur();
+        ah1.setHuurder(k1);
+        ah1.setGehuurdeAuto(a1);
+        ah1.setAantalDagen(100);
+
+        assertEquals(15000, ah1.totaalPrijs());
     } 
 
     @Test
-    public void TestNullInAutoHuur(){
+    public void TestToStingNullInAutoEnHuurder(){
         AutoHuur ah1 = new AutoHuur();
 
         assertEquals(0.0, ah1.totaalPrijs());
         assertEquals("er is geen auto bekend"+ "\n" + "er is geen huurder bekend" + "\n" + "aantal dagen: 0 en dat kost: 0.0", ah1.toString());
     }
+
+    @Test
+    public void TestToStringNullInAuto(){
+        
+        Klant k1 = new Klant("John Doe");
+        AutoHuur ah1 = new AutoHuur();
+        ah1.setHuurder(k1);
+
+        assertEquals(0.0, ah1.totaalPrijs());
+        assertEquals("er is geen auto bekend"+ "\n" + "op naam van: John Doe (korting: 0.0%)" + "\n" + "aantal dagen: 0 en dat kost: 0.0", ah1.toString());
+    }
+
+    @Test
+    public void TestToStringNullInHuurder(){
+        
+        Auto a1 = new Auto("Audi A4", 50.0);
+        AutoHuur ah1 = new AutoHuur();
+        ah1.setGehuurdeAuto(a1);
+
+        assertEquals(0.0, ah1.totaalPrijs());
+        assertEquals("auto type: Audi A4 met prijs per dag: 50.0"+ "\n" + "er is geen huurder bekend" + "\n" + "aantal dagen: 0 en dat kost: 0.0", ah1.toString());
+    }
+
+    @Test
+    public void TestToStringGeenNull(){
+        
+        Auto a1 = new Auto("Audi A4", 50.0);
+        Klant k1 = new Klant("John Doe");
+        AutoHuur ah1 = new AutoHuur();
+        ah1.setGehuurdeAuto(a1);
+        ah1.setHuurder(k1);
+
+        assertEquals(0.0, ah1.totaalPrijs());
+        assertEquals("auto type: Audi A4 met prijs per dag: 50.0"+ "\n" + "op naam van: John Doe (korting: 0.0%)" + "\n" + "aantal dagen: 0 en dat kost: 0.0", ah1.toString());
+    }
+
 } 
